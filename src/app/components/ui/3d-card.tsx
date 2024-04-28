72d575426698a71638bf95f7733da53e644856db
Niyander069 <bugga0377@gmail.com>
2024-04-28 18:46:40 +0530

    Update 3d-card.tsx

--- src/app/components/ui/3d-card.tsx
+++ src/app/components/ui/3d-card.tsx
@@ -136,6 +136,8 @@ export const CardItem = ({
 
   useEffect(() => {
     handleAnimations();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+
   }, [isMouseEntered, handleAnimations]);
   
   
